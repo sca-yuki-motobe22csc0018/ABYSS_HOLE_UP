e6ff97a0cc6da809fb5536803b4ff83d83bc5c24
Yuki <sdt22csc0018@edu.sca.ac.jp>
2024-11-16 15:38:24 +0900

    プレイヤーの動きおｋ

--- Assets/Scenes/PlayerMove.cs
+++ Assets/Scenes/PlayerMove.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -15,12 +16,20 @@ public class PlayerMove : MonoBehaviour
     public GameObject PlayerSkin;
     [SerializeField] Rigidbody2D rb = new Rigidbody2D();
     public float PowerMaxTime;
+    public float PowerCoolTime;
     float PowerTimer;
     float PowerTimeStock;
     private Vector3 moveDirection; // 移動方向
+    public Image gaugeImage; // ゲージに使用するImageコンポーネント
+    [Range(0, 1)] public float fillAmount = 1.0f; // 塗りつぶし率
+    bool move;
+    public Color fullHealthColor = Color.white;   // HP最大時の色
+    public Color lowHealthColor = Color.red;     // HP最小時の色
+    public float forceMultiplier = 10f; // 力の大きさを調整
     // Start is called before the first frame update
     void Start()
     {
+        move = false;
         PowerTimer = 0;
         rb = GetComponent<Rigidbody2D>();
         this.transform.position = StartPosition;
@@ -29,19 +38,40 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0)&&PowerTimer<PowerMaxTime)
+        if (gaugeImage != null)
+        {
+            gaugeImage.fillAmount = fillAmount;
+        }
+        if (Input.GetMouseButton(0)&&PowerTimer<PowerMaxTime&&!move)
         {
             PowerTimer += Time.deltaTime;
         }
         if (Input.GetMouseButtonUp(0))
         {
-            // カメラからクリック位置へのRayを作成
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            PowerTimer = 0;
-            RaycastHit hit;
-            // ヒットした位置から方向を計算
-            moveDirection = (hit.point - transform.position).normalized;
+            move = true;
+            // マウスのワールド座標を取得
+            Vector3 mouseP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseP.z = 0; // 2DのためZ軸を0に
+
+            // クリックした方向を計算
+            Vector2 dir = (mouseP - transform.position).normalized;
+
+            // Rigidbody2Dに力を加える
+            rb.AddForce(dir * PowerTimer/4, ForceMode2D.Impulse);
         }
+        if (move)
+        {
+            PowerTimer -= PowerCoolTime * Time.deltaTime;
+            gaugeImage.color = lowHealthColor;
+        }
+        if (PowerTimer <= 0)
+        {
+            PowerTimer = 0;
+            move = false;
+            gaugeImage.color = fullHealthColor;
+        }
+        SetGauge(PowerTimer/PowerMaxTime);
+
         // マウスのスクリーン座標を取得
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0f; // 2DなのでZ座標は無視
@@ -51,18 +81,28 @@ public class PlayerMove : MonoBehaviour
 
         // ベクトルの角度を取得して回転させる
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        PlayerSkin.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-        if (PlayerSkin.transform.rotation.z > 0.7f || PlayerSkin.transform.rotation.z < -0.7f)
+        
+        if (!move)
         {
-            PlayerSkin.transform.localScale = new Vector3(-0.5f, -0.5f, 1);
+            PlayerSkin.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            if (PlayerSkin.transform.rotation.z > 0.7f || PlayerSkin.transform.rotation.z < -0.7f)
+            {
+                PlayerSkin.transform.localScale = new Vector3(-0.5f, -0.5f, 1);
+            }
+            else
+            {
+                PlayerSkin.transform.localScale = new Vector3(-0.5f, 0.5f, 1);
+            }
         }
-        else
-        {
-            PlayerSkin.transform.localScale = new Vector3(-0.5f, 0.5f, 1);
-        }
+        
         
     }
 
+    // 任意でゲージを設定するメソッド
+    public void SetGauge(float value)
+    {
+        fillAmount = Mathf.Clamp01(value); // 0～1にクランプ
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {/*
         if (collision.gameObject.CompareTag("Wall"))
